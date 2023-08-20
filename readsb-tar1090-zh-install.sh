fb4e0ad5ec23513ee3026ec0fc020bb2b93ac64c
FAN <236659588@qq.com>
2023-08-20 15:03:46 +0800

    更新 readsb-tar1090-zh-install.sh

--- readsb-tar1090-zh-install.sh
+++ readsb-tar1090-zh-install.sh
@@ -258,13 +258,9 @@ wget -P /etc https://ghproxy.com/https://github.com/HLLF-FAN/ADSB-scripts/blob/m
 echo
 echo "             readsb+tar1090 已经安装完成！但是目前 readsb 服务未运行！"
 echo "                       因此需要重启设备以启动 readsb 服务"
-echo "                 tar1090的Web页面为 http://$(ip route get 1.2.3.4 | grep -m1 -o -P 'src \K[0-9,.]*')/"
+echo "                 tar1090的Web页面为 http://$(ip route get 1.2.3.4 | grep -m1 -o -P 'src \K[0-9,.]*')/tar1090"
 echo "                     Web界面将显示错误，直至 readsb 正常运行"
 echo
-echo
-echo "                     不要忘记使用十进制纬度和经度设置您的位置"
-echo "                                    例如:"
-echo "                     readsb-set-location 50.12344 10.23429"
 echo 
 echo "                               ！！！注意！！！ "        
 echo "                             请不要尝试将数据传给"
