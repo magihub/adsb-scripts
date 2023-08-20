6aca2431290049fcab95a6be97397dcbc7eac734
FAN <107034178+HLLF-FAN@users.noreply.github.com>
2023-08-20 13:17:55 +0800

    更新 scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -28,7 +28,7 @@ while true; do
     case $choice in
     1)
         echo "安装 readsb + tar1090 （中文版）"
-        bash -c "$(wget -nv -O - https://ghproxy.com/https://github.com/HLLF-FAN/tar1090/raw/master/readsb-install-cn.sh)"
+        bash -c "$(wget -nv -O - https://ghproxy.com/https://github.com/HLLF-FAN/ADSB-script/raw/master/readsb-install-cn.sh)"
         exit
         ;;
     2)
