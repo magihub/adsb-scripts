740341bec28913a27db66185acd60f67acce0755
magihub <77531918+magihub@users.noreply.github.com>
2024-03-21 22:59:09 +0800

    Add files via upload

---
+++ osm_tiles_offline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
+
+IPATH=/usr/local/share/osm_tiles_offline
+
+mkdir -p "$IPATH"
+cd "$IPATH"
+
+F=osm_tiles_0_9.tar.gz
+wget -O $F https://github.com/wiedehopf/adsb-scripts/releases/download/osm2022/$F
+
+echo Extracting, this will take a bit .....
+tar --overwrite -x -f $F
+rm -f $F
+
+echo ................................. done
